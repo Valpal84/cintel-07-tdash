3f9e37c73ee526ce8bd0957fc84d9c75c2866d44
Valpal84 <142853655+Valpal84@users.noreply.github.com>
2024-04-17 21:03:06 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,16 +1,23 @@
+# Install necessary imports
 import seaborn as sns
 from faicons import icon_svg
 
 from shiny import reactive
 from shiny.express import input, render, ui
 import palmerpenguins 
+from shinyswatch import theme
 
+#Load the necessary dataset into a dataframe
 df = palmerpenguins.load_penguins()
 
-ui.page_opts(title="Penguins dashboard", fillable=True)
+#Create the overall page options, add title
+ui.page_opts(title="Valerie's Penguin dashboard", fillable=True)
 
+#Add a theme to the page to change the appearance
+theme.darkly()
 
-with ui.sidebar(title="Filter controls"):
+#Create a sidebar with a slider, and checkbox group
+with ui.sidebar(title="Customizable Filter Controls"):
     ui.input_slider("mass", "Mass", 2000, 6000, 6000)
     ui.input_checkbox_group(
         "species",
@@ -18,8 +25,10 @@ with ui.sidebar(title="Filter controls"):
         ["Adelie", "Gentoo", "Chinstrap"],
         selected=["Adelie", "Gentoo", "Chinstrap"],
     )
+
+    #Add pertinent links to assist users
     ui.hr()
-    ui.h6("Links")
+    ui.h6("Helpful Links")
     ui.a(
         "GitHub Source",
         href="https://github.com/denisecase/cintel-07-tdash",
@@ -47,7 +56,7 @@ with ui.sidebar(title="Filter controls"):
         target="_blank",
     )
 
-
+#Add value boxes to display important information including icons.
 with ui.layout_column_wrap(fill=False):
     with ui.value_box(showcase=icon_svg("earlybirds")):
         "Number of penguins"
@@ -70,11 +79,15 @@ with ui.layout_column_wrap(fill=False):
         def bill_depth():
             return f"{filtered_df()['bill_depth_mm'].mean():.1f} mm"
 
-
+#Add cards to display filtered information. 
 with ui.layout_columns():
+    ui.tags.style(
+        ".card-header {color:white; background:#527490 !important; }"
+    )
     with ui.card(full_screen=True):
         ui.card_header("Bill length and depth")
 
+        #Create a scatterploy to display selected controls and filtered data.
         @render.plot
         def length_depth():
             return sns.scatterplot(
@@ -84,8 +97,9 @@ with ui.layout_columns():
                 hue="species",
             )
 
+    #Create a Data Grid with Penguin information.
     with ui.card(full_screen=True):
-        ui.card_header("Penguin da")
+        ui.card_header("Penguin Data")
 
         @render.data_frame
         def summary_statistics():
@@ -101,7 +115,7 @@ with ui.layout_columns():
 
 #ui.include_css(app_dir / "styles.css")
 
-
+#Using reactive calc, define a filtered dataframe, and return the dataframe.
 @reactive.calc
 def filtered_df():
     filt_df = df[df["species"].isin(input.species())]
